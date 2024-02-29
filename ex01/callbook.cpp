93f9db5805e1014403bcc2628eef381028007bfb
alecmalloc <aeastman@student.42berlin.de>
2024-02-29 14:02:33 +0100

    added basic search screen

--- ex01/callbook.cpp
+++ ex01/callbook.cpp
@@ -1,23 +1,62 @@
 #include "callbook.hpp"
-#include <limits>
+#include <iomanip>
 
-void	book_search(void)
+#include <iomanip> // Include the <iomanip> library for std::setw
+
+void	display_header(void)
 {
+	std::cout << std::setw(10) << std::right << "index";
+	std::cout << "|";
+	std::cout << std::setw(10) << std::right << "first name"; // Increase the width to accommodate longer names
+	std::cout << "|";
+	std::cout << std::setw(10) << std::right << "last name"; // Increase the width to accommodate longer names
+	std::cout << "|";
+	std::cout << std::setw(10) << std::right << "nickname"; // Increase the width to accommodate longer nicknames
+	std::cout << std::endl;
+}
+
+void	display_entries(PhoneBook &Pb)
+{
+	for (int i = 0; !Pb.contacts[i].first_name.empty(); i++)
+	{
+		std::cout << std::setw(10) << std::right << i;
+		std::cout << "|";
+		std::cout << std::setw(10) << std::right << Pb.contacts[i].first_name;
+		std::cout << "|";
+		std::cout << std::setw(10) << std::right << Pb.contacts[i].last_name;
+		std::cout << "|";
+		std::cout << std::setw(10) << std::right << Pb.contacts[i].nick_name;
+		std::cout << std::endl;
+	}
+}
+
+void	book_search(PhoneBook &Pb)
+{
+	display_header();
+	display_entries(Pb);
 	return ;
 }
 
-int		index_selector(PhoneBook Pb)
+void	bop_and_shift_up(PhoneBook &Pb)
+{
+	for (int i = 0; i < 7; i++)
+		Pb.contacts[i] = Pb.contacts[i + 1];
+}
+
+int		index_selector(PhoneBook &Pb)
 {
 	for (int i = 0; i < 8; i++)
 	{
 		if (Pb.contacts[i].first_name.empty())
 			return (i);
 	}
-	return (0);
+	bop_and_shift_up(Pb);
+	return (7);
 }
 void	book_add(PhoneBook &Pb)
 {
 	int i =	index_selector(Pb);
+	std::cout << "working on: " << i << std::endl;
 	std::string buffer;
 
 	std::cout << "first name:" << std::endl;
@@ -43,7 +82,7 @@ int main()
 		if (buffer == "EXIT")
 			return (1);
 		if (buffer == "SEARCH")
-			book_search();
+			book_search(Pb);
 		if (buffer == "ADD")
 			book_add(Pb);
 	}
